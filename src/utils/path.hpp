ac59bb6761d425f517567986f4a54f4df54dcd2e
Felix Heitmann <felixjulianheitmann@gmail.com>
2022-12-02 20:00:55 +0100

    Updated solutions path

--- src/utils/path.hpp
+++ src/utils/path.hpp
@@ -2,14 +2,14 @@
 
 #include <filesystem>
 
-#ifndef AOC_SRC_DIR_PATH
-#define AOC_SRC_DIR_PATH "."
+#ifndef AOC_ROOT_DIR_PATH
+#define AOC_ROOT_DIR_PATH "."
 #endif
 
 namespace utils {
-    inline auto get_src_dir() -> std::filesystem::path { return AOC_SRC_DIR_PATH; }
+    inline auto get_src_dir() -> std::filesystem::path { return std::filesystem::path( AOC_ROOT_DIR_PATH ) / "src"; }
     inline auto get_solutions_dir() -> std::filesystem::path
     {
-        return std::filesystem::path( AOC_SRC_DIR_PATH ) / "solutions";
+        return std::filesystem::path( AOC_ROOT_DIR_PATH ) / "solutions";
     }
 }  // namespace utils
