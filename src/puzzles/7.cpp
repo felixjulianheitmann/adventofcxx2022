e2489c182a5aa339e1579ebf2afdc364bf2ad9b3
Felix Heitmann <felix@tessonics.com>
2022-12-07 09:58:00 +0100

    More on Puzzle 7

--- src/puzzles/7.cpp
+++ src/puzzles/7.cpp
@@ -1,20 +1,22 @@
 #include <puzzles.hpp>
+#include <unordered_map>
 #include <variant>
 
 class file;
 class directory;
 using node = std::variant< file, directory >;
 
-class file {
+struct file {
     std::string name;
     std::size_t size;
-    auto        size() const -> std::size_t { return size; }
 };
 
-class directory {
-    std::string         name;
-    std::vector< node > subnodes;
-    auto                size() -> std::size_t
+struct directory {
+    std::string                             name;
+    std::unordered_map< std::string, node > subnodes;
+    directory *                             parent;
+
+    auto size() -> std::size_t
     {
         std::size_t size = 0;
         for ( auto const & n : subnodes ) {
@@ -38,7 +40,46 @@ void p7::puzzle( std::filesystem::path const & src_data )
 {
     auto lines = utils::read_lines< std::string >( src_data );
 
+    auto access_path = []( node & root, std::ranges::forward_range auto const & path ) {
+        auto n = &root;
+        for ( auto const & p : path ) {
+            n = &std::get_if< directory >( &n )->subnodes[p];
+        }
+        return *n;
+    };
+
+    std::vector< std::string > path;
+
+    directory root{
+        .name     = "/",
+        .subnodes = {},
+        .parent   = nullptr,
+    };
+    directory * current_dir = &root;
     for ( auto const & line : lines ) {
+        if ( line.starts_with( "$ cd" ) ) {
+            auto sub_path = line.substr( 5 );
+            if ( sub_path == ".." ) {
+                path.pop_back();
+                current_dir = current_dir->parent;
+            }
+            else {
+                current_dir->subnodes[sub_path] = directory{
+                    .name     = sub_path,
+                    .subnodes = {},
+                    .parent   = current_dir,
+                };
+                path.push_back( sub_path );
+                current_dir = &current_dir->subnodes[sub_path];
+            }
+        }
+        else if ( line.starts_with( "$ ls" ) ) {
+            continue;
+        }
+        else {
+            auto split                          = utils::split_as< std::string >( std::string_view( line ), " " );
+            current_dir->subnodes[split.back()] = file { .name = split.back(), .size = std::stol( split.front() ); };
+        }
     }
 
     utils::answer( "7_1", 0 );
